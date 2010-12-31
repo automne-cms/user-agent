b4df7d311bb9fcbd666cb64b95a408f95ec9e2d9
sebastien <sebastien.pauchet@ws-interactive.fr>
2010-12-31 09:42:21 +0100

    [REFACTOR] : Add support for sub folder installation

--- cms_ua_install.php
+++ cms_ua_install.php
@@ -4,7 +4,7 @@
   * @author Sébastien Pauchet <sebastien.pauchet@ws-interactive.fr>
   */
 
-require_once($_SERVER["DOCUMENT_ROOT"]."/cms_rc_admin.php");
+require_once(dirname(__FILE__).'/../../cms_rc_admin.php');
 
 //check if CMS_ua is already installed (if so, it is an update)
 $sql = "select 1 from modules where codename_mod = 'cms_ua'";
