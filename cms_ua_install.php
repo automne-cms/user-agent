b771390eaf626075f7def5250798b7047d55060c
sebastien-pauchet <seb@automne-cms.org>
2011-05-27 14:14:04 +0200

    Correct a bug in install process

--- cms_ua_install.php
+++ cms_ua_install.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__).'/../../cms_rc_admin.php');
 //check if module is already installed (if so, it is an update)
 $installed = false;
 $codenames = CMS_modulesCatalog::getAllCodenames(true);
-if (isset($modules['cms_ua'])) {
+if (isset($codenames['cms_ua'])) {
 	$installed = true;
 }
 
